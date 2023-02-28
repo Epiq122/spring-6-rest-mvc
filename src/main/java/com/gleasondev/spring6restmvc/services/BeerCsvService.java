9aa626c70fa9a75bd054de45fe65e78c5e3c35cc
Robert Gleason <l2ob122@gmail.com>
2023-02-27 21:39:58 -0800

    created csv services and test

---
+++ src/main/java/com/gleasondev/spring6restmvc/services/BeerCsvService.java
@@ -0,0 +1,1 @@
+package com.gleasondev.spring6restmvc.services;import com.gleasondev.spring6restmvc.model.BeerCSVRecord;import java.io.File;import java.util.List;public interface BeerCsvService {    List<BeerCSVRecord> convertCSV(File csvFile);}
